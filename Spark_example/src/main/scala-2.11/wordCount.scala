c07d76335898c72ffb5105b316b1eb55b186eaa1
Lucie Burgess <lucie.burgess@icloud.com>
2017-08-03 22:59:19 +0100

    Got Spark working with IntelliJ and Scala - see build.sbt Updated .gitignore to incorporate IntelliJ files and sbt files, etc.

---
+++ Spark_example/src/main/scala-2.11/wordCount.scala
@@ -0,0 +1,31 @@
+/**
+  * Created by lucieburgess on 03/08/2017.
+  */
+
+import org.apache.spark.{SparkContext, SparkConf}
+
+object wordCount {
+
+  def main(args: Array[String]): Unit = {
+     val conf = new SparkConf() // defining Spark configuration object
+        .setMaster("local[*]") // set SparkMaster as local
+        .setAppName("Spark test")
+        .set("spark.executor.memory", "4g")
+
+    val sc = new SparkContext(conf)
+
+    val t1 = System.nanoTime()
+
+    // Defines the parallel action on the spark context
+    val lines = sc.parallelize(Seq("This is the first line", "This is the second line", "This is the third line"))
+
+    val counts = lines.flatMap(line => line.split(" "))
+                      .map(word => (word, 1))
+                      .reduceByKey(_+_)
+      counts.foreach(println)
+
+    val duration = (System.nanoTime() - t1)/ 1e9d
+
+    println("****************"+duration+"******************")
+  }
+}
---
+++ Spark_example/src/main/scala-2.11/wordCountNotParallel.scala
@@ -0,0 +1,25 @@
+
+/**
+  * Created by lucieburgess on 03/08/2017.
+  */
+object wordCountNotParallel {
+
+    def reduceByKey[K,V](collection: Traversable[Tuple2[K,V]])(implicit num: Numeric[V]) = {
+      import num._
+      collection
+        .groupBy(_._1)
+        .map { case (group: K, traversable) => traversable.reduce { (a, b) => (a._1, a._2 + b._2) }
+        }
+    }
+
+    def main(args: Array[String]): Unit = {
+
+      // Defines the parallel action on the spark context
+      val lines = Seq("This is the first line", "This is the second line", "This is the third line")
+
+
+      val counts = lines.flatMap(line => line.split(" ")).map(word => (word, 1))
+      reduceByKey(counts)
+      counts.foreach(println)
+    }
+}
